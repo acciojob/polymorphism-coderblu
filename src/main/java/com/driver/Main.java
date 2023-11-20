d2bee2f984b55c781dafbd6072ec74f9be1638da
unknown <akashsahu786aim@gmmail.com>
2023-11-20 12:02:27 +0530

    Completed

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,33 @@
 package com.driver;
 
 public class Main {
+    public Main() {
+        System.out.println("Main Class");
+    }
 
+    public void main(String[] args) {
+        Product p = new Product();
+        p.product(4,5);
+        p.product((long) 4.00,(long)5.00);
+        p.product(4,5,7);
+    }
+
+    class Product
+    {
+        public Product() {
+        }
+
+        public void product(int a, int b)
+        {
+
+        }
+        public void product(int a, int b, int c)
+        {
+
+        }
+        public void product(long a, long b)
+        {
+
+        }
+    }
 }
